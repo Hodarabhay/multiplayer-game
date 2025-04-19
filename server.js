8786b5b7fef1bf065da097acd6bc836388705e62
hodarabhay <hodarabhay553@gmail.com>
2025-04-19 14:16:13 +0530

    fix: added root route for index.html

--- server.js
+++ server.js
@@ -21,3 +21,19 @@ io.on('connection', (socket) => {
 http.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Serve static files from "public" folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Fix: Root route ('/')
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
